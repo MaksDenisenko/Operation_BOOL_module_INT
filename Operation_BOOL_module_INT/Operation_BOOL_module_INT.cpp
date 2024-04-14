56988e84ac3ddb04b947e49898471e54feba357b
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-04-14 12:50:26 +0300

    Добавьте файлы проекта.

---
+++ Operation_BOOL_module_INT/Operation_BOOL_module_INT.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    bool flag = false;
+    int32_t x;
+    std::cin >> x;
+    while (x != 0) {
+        if (x % 10 == 7)
+            flag = true;
+        x = x / 10;
+    }
+    std::cout << flag;
+}
+
